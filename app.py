e749384492cc4a27972d21f5053879129acacf79
VJL0 <jlvictor2020@gmail.com>
2024-08-30 01:00:52 -0400

    added fucntion to get programs/links

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+from temple_requests import get_academic_programs
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
+@app.route("/academic-programs")
+def get_academic_programs_wrapper():
+    data = get_academic_programs()
+    return jsonify(data)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
